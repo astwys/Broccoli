594c656a43f8f270cdcb20b426d6560ebe0714bf
astwys <astwys@gmail.com>
2015-12-05 16:36:29 +0100

    added update method in Downloader.cs

--- Broccoli/Broccoli/Downloader.cs
+++ Broccoli/Broccoli/Downloader.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Broccoli
 {
     class Downloader
     {
-    }
+      public Model Model{ get; set; }
+
+      public Downloader(Model model){
+          Model = model;
+      }
+
+      private XDocument update() {
+          string xml = new WebClient().DownloadString("http://www.theverge.com/rss/index.xml");
+          XDocument doc = XDocument.Parse(xml);
+          return doc;
+      }
+
+
+  }
 }
