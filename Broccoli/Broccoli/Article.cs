9d1f4a722b736a3a5046906f1da910d4b16a51b9
astwys <astwys@gmail.com>
2015-12-07 15:20:16 +0100

    wrote Article class; started with process method in Downloader.cs

--- Broccoli/Broccoli/Article.cs
+++ Broccoli/Broccoli/Article.cs
@@ -8,5 +8,19 @@ namespace Broccoli
 {
     class Article
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+
+        public Article (int id, string title, string link) {
+            ID = id;
+            Title = title;
+            Link = link;
+        }
+
+        public override string ToString()
+        {
+            return ID + ": " + Title;
+        }
     }
 }
